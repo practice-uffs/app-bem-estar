5adb336f58aa6d7905921aa4676370de400e2b0c
Stefani Meneghetti <meneghettistefani@gmail.com>
2021-11-30 11:19:56 -0300

    feat: separa o storage e a api em classes diferentes

---
+++ src/js/api.js
@@ -0,0 +1,69 @@
+export class Api {
+    constructor(app) {
+        this.app = app;
+        this.app.api = this;
+
+        const settings = this.app.storage.getSettings();
+        if (settings.devMode && settings.testApi) {
+            this.url = this.app.storage.testApiURL;
+        } else {
+            this.url = this.app.storage.prodApiURL;
+        } 
+    }
+
+    async login(username, password) {
+        var self = this;
+        var app = self.app;
+
+        return await app.request.promise.post(app.api.url + "auth", {
+            "user": username,
+            "password": password,
+            "app_id": "1"
+        }).then( async (res) => {
+            let data = JSON.parse(res.data);
+            app.storage.setUserData(data.user);
+            if (data.passport) {
+                app.storage.setUserCredentials(data);
+                app.request.setup({
+                    headers: {
+                        Authorization: "Bearer " + data.passport,
+                    },
+                });
+                return true;
+            } else {
+                return false;
+            }
+        }).catch((err) => {
+            return false;
+        });
+    };
+
+    logout () {
+		storage.removeAllButUserData();
+        storage.clearUserCredentials();
+	}
+
+    async requestUserData() {
+        var self = this;
+        var app = self.app;
+
+        return await app.request.promise.post(app.api.url + "user")
+        .then((res) => {
+            let data = JSON.parse(res.data);
+            if(data.error){
+                this.requestLogout().then(res => {
+                    if (res) {
+                        app.dialog.alert(
+                            "Sessão expirada ou inválida, faça login novamente!"
+                        );
+                        app.views.main.router.navigate("/");
+                    }
+                });
+                return;
+            }
+            const userData = JSON.parse(res.data);
+            app.storage.setUserData(userData);
+            return userData;
+        });
+    };
+}
